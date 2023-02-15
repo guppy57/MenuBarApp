e2826f04920cd519ec57c46f9fcf0789c8fe2078
Armaan Gupta <armaan@guppy.im>
2023-02-15 01:14:40 -0600

    Initial Commit

---
+++ MenuBarApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MenuBarApp
+//
+//  Created by Armaan Gupta on 2/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MenuBarApp/MenuBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarAppApp.swift
+//  MenuBarApp
+//
+//  Created by Armaan Gupta on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
