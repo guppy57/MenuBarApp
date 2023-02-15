ec07462dd1085d71668b62c7dc498e159f1a294a
Armaan Gupta <armaan@guppy.im>
2023-02-15 01:31:38 -0600

    added keyboard shortcuts and quit option

--- MenuBarApp/MenuBarAppApp.swift
+++ MenuBarApp/MenuBarAppApp.swift
@@ -9,25 +9,33 @@ import SwiftUI
 
 @main
 struct swiftui_menu_barApp: App {
-    // 1
     @State var currentNumber: String = "1"
     
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        // 2
         MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
-            // 3
-            Button("Guppy") {
+            Button("One") {
                 currentNumber = "1"
             }
+            .keyboardShortcut("1")
             Button("Two") {
                 currentNumber = "2"
             }
+            .keyboardShortcut("2")
             Button("Three") {
                 currentNumber = "3"
             }
+            .keyboardShortcut("3")
+            Divider()
+
+            Button("Quit") {
+
+                NSApplication.shared.terminate(nil)
+
+            }.keyboardShortcut("q")
+
         }
     }
 }
